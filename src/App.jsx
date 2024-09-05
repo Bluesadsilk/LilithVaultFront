15209e9c3dbf8e305b4c3258d240c893eef9d1a1
BlueKorpse <haxansword@gmail.com>
2024-09-05 18:08:35 +0200

    Creating products

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-
+import Product from './components/Product'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Lilith Vault</h1> 
+      <h1>Lilith Vault</h1>
+      <Product/>      
     </>
   )
 }
---
+++ src/components/Product.jsx
@@ -0,0 +1,13 @@
+import {React, useState} from "react";
+
+function Product(){
+
+
+    return(
+    <>
+    <p>Hola soy un producto</p>
+    </>
+    )
+}
+
+export default Product
